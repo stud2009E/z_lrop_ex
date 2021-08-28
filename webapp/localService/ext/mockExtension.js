10ff42b08bfb9441dff597e59a9563c9c0b4b98a
unknown <stud2009@yandex.ru>
2021-08-28 17:26:31 +0300

    add FI PrcessHeader mockserver handling

--- webapp/localService/ext/mockExtension.js
+++ webapp/localService/ext/mockExtension.js
@@ -10,10 +10,44 @@ sap.ui.define(function(){
 		 * @public
 		 * @see {@link /workspace/mockservers/mockserver.js}
 		 */
-		apply(mockserver){
+		apply: function(mockserver){
 
-			//here goes your setup
+			var aRequests = mockserver.getRequests();
+			
+			aRequests.push({
+				method: "POST",
+				path: new RegExp("ProcessHeader.*"),
+				response: function(oXhr){
+					var oGuidRegexp = /([0-9A-Fa-f]{8}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{12})/;
+					var sGuid = oGuidRegexp.exec(oXhr.url);
+					var sRootUri = mockserver.getRootUri();
+					if(sGuid){
+						sGuid = sGuid[0];
+					}else{
+						oXhr.respond(500);
+						return true;
+					}
 
+					var aHeaderItem = mockserver._oMockdata.HeaderSet.find(item => item.Guid === sGuid);
+
+					jQuery.ajax({
+						url: `${sRootUri}HeaderSet(guid'${sGuid}')`,
+						dataType : 'json',
+						async: false,
+						success : function(oData) {
+							if(oData && oData.d && oData.d.Decimal){
+								oData.d.Decimal *= 2;
+								aHeaderItem.Decimal = oData.d.Decimal;
+							}
+							oXhr.respondJSON(200, {}, JSON.stringify(oData));
+						}
+					});
+
+					return true;
+				}
+			});
+
+			mockserver.setRequests(aRequests);
 		}
 	};
 
