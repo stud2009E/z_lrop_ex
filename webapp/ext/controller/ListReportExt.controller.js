4edbc24f02503f281d4329b07b0b27d1e23f7813
unknown <stud2009@yandex.ru>
2021-09-01 00:51:35 +0300

    create object with predefined params

--- webapp/ext/controller/ListReportExt.controller.js
+++ webapp/ext/controller/ListReportExt.controller.js
@@ -8,6 +8,17 @@ sap.ui.controller("z.lrop.ex.ext.controller.ListReportExt", {
 
 	onBeforeRebindTableExtension: function(oEvent){
 		
+	},
+
+
+	getPredefinedValuesForCreateExtension: function(oSmartFilterBar){
+		return {
+			String: "Test",
+			Status: "00",
+			Int16: null,
+			Decimal: null,
+			Int32: null
+		};
 	}
 
 });
